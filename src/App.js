cf9dc95cf091a4adaabcd597a43f230b244317fb
shadab ahmed <shadab33@hotmail.com>
2022-10-09 02:21:06 +0400

    building Airbnb clone in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import Data from "./data"
+import Styles from "./App.css"
+export default function App() {
+    const Cards = Data.map(item =>{
+     return   <Card 
+                key={item.id}
+                {...item}
+            />
+    })
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+            <section className="cards-list">
+                {Cards}
+            </section>
+        </div>
+    )
 }
-
-export default App;
